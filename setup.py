46f81500a6ff0f6b20cc42aad2f3b147b1f45326
Do Hoerin <hellodhlyn@gmail.com>
2019-10-25 02:45:00 +0900

    Updating to v0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='telegrambot-py',
-    version='0.0.1',
+    version='0.1.0',
     description='Make your own telegram bot easily',
     long_description=long_description,
     long_description_content_type='text/markdown',
